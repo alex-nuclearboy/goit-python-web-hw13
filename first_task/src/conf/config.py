1529d211edbb190ac807f41e3fb4227e61f35d7b
Aleksander <alex.nuclearboy@gmail.com>
2024-05-12 19:24:01 +0200

    Add environment variables to the configuration file

--- first_task/src/conf/config.py
+++ first_task/src/conf/config.py
@@ -1,13 +1,19 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
+from pydantic import EmailStr
 
 
 class Settings(BaseSettings):
+    postgres_db: str
+    postgres_user: str
+    postgres_password: str
+    postgres_host: str = 'localhost'
+    postgres_port: int = 5432
     sqlalchemy_database_url: str
     secret_key: str
     algorithm: str
     mail_username: str
     mail_password: str
-    mail_from: str
+    mail_from: EmailStr
     mail_port: int
     mail_server: str
     redis_host: str = 'localhost'
