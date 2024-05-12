286c5d3cce5efbe78c8b8b05297cde58b2d5637b
Aleksander <alex.nuclearboy@gmail.com>
2024-05-12 12:01:58 +0200

    Import the declarative DB and the string to connect to the DB

--- first_task/migrations/env.py
+++ first_task/migrations/env.py
@@ -5,6 +5,9 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from src.database.models import Base
+from src.database.db import SQLALCHEMY_DATABASE_URL
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -18,7 +21,10 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+# target_metadata = None
+target_metadata = Base.metadata
+
+config.set_main_option("sqlalchemy.url", SQLALCHEMY_DATABASE_URL)
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
