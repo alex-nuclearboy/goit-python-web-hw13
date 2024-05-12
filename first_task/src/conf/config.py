558f41d7b70ee51e255e3b3d1c1e167424c616ff
Aleksander <alex.nuclearboy@gmail.com>
2024-05-12 15:49:47 +0200

    Add the information to the environment configuration file
    Added information about the database connection, secret key, algorithm, and email server data to the configuration file

--- first_task/src/conf/config.py
+++ first_task/src/conf/config.py
@@ -2,6 +2,14 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
+    sqlalchemy_database_url: str
+    secret_key: str
+    algorithm: str
+    mail_username: str
+    mail_password: str
+    mail_from: str
+    mail_port: int
+    mail_server: str
     redis_host: str = 'localhost'
     redis_port: int = 6379
 
