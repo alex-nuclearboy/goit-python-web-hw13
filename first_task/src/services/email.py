1ab35a2716117d12c56404895f83ef24f3e85b40
Aleksander <alex.nuclearboy@gmail.com>
2024-05-12 16:18:03 +0200

    Fix

--- first_task/src/services/email.py
+++ first_task/src/services/email.py
@@ -26,9 +26,13 @@ async def send_email(email: EmailStr, username: str, host: str):
     try:
         token_verification = auth_service.create_email_token({"sub": email})
         message = MessageSchema(
-            subject="Confirm your email ",
+            subject="Confirm your email",
             recipients=[email],
-            template_body={"host": host, "username": username, "token": token_verification},
+            template_body={
+                "host": host,
+                "username": username,
+                "token": token_verification
+            },
             subtype=MessageType.html
         )
 
@@ -36,4 +40,3 @@ async def send_email(email: EmailStr, username: str, host: str):
         await fm.send_message(message, template_name="email_template.html")
     except ConnectionErrors as err:
         print(err)
-
