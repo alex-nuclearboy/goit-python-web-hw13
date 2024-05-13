f323aba4c7b06a9187af9ba4f05217738f834861
Aleksander <alex.nuclearboy@gmail.com>
2024-05-13 20:38:47 +0200

    Add a new route

--- first_task/main.py
+++ first_task/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from fastapi_limiter import FastAPILimiter
 from fastapi.middleware.cors import CORSMiddleware
 
-from src.routes import contacts, auth
+from src.routes import contacts, auth, users
 from src.conf.config import settings
 
 app = FastAPI()
@@ -22,6 +22,7 @@ app.add_middleware(
 
 app.include_router(auth.router, prefix='/api')
 app.include_router(contacts.router, prefix='/api')
+app.include_router(users.router, prefix='/api')
 
 
 @app.on_event("startup")
