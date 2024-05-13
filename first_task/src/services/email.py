5dbf24f6e7d5bbcf27a760f7adf882e8effb229e
Aleksander <alex.nuclearboy@gmail.com>
2024-05-13 19:36:58 +0200

    Add a function to reset the password

--- first_task/src/services/email.py
+++ first_task/src/services/email.py
@@ -46,15 +46,15 @@ async def send_email(email: EmailStr, username: str, host: str):
                     for email verification.
 
     Raises:
-        HTTPException: If the email could not be sent,
-                       possibly due to connection issues.
+        HTTPException: An error occurs if the email could not be sent due to
+                       server or configuration issues.
     """
     try:
         # Generate a verification token for the email
         token_verification = auth_service.create_email_token({"sub": email})
         # Prepare the email message with a template
         message = MessageSchema(
-            subject="Confirm your email",
+            subject="Confirm Your Email",
             recipients=[email],  # List of recipients
             template_body={
                 "host": str(host),
@@ -67,6 +67,46 @@ async def send_email(email: EmailStr, username: str, host: str):
         # Send the email using the configured FastMail instance
         fm = FastMail(conf)
         await fm.send_message(message, template_name="email_template.html")
+    except ConnectionErrors as err:
+        print(f"Failed to send email: {err}")
+        raise HTTPException(status_code=500, detail="Email could not be sent.")
+
+
+async def send_reset_email(email: EmailStr, username: str, host: str):
+    """
+    Sends a password reset email to a specified user.
+
+    This function generates a token for password resetting and sends an email
+    containing a token that the user can use to verify their identity and
+    reset their password through a specified interface, such as Swagger.
+
+    Args:
+        email (EmailStr): The email address of the user who requested
+                          a password reset.
+        username (str): The username of the user to whom the email
+                        will be addressed.
+        host (str): The base URL of the server where the password reset
+                    can be processed.
+
+    Raises:
+        HTTPException: An error occurs if the email could not be sent due to
+                       server or configuration issues.
+    """
+    try:
+        token_verification = auth_service.create_email_token({'sub': email})
+        message = MessageSchema(
+            subject="Password Reset Request",
+            recipients=[email],
+            template_body={
+                "host": str(host),
+                "username": username,
+                "token": token_verification
+            },
+            subtype=MessageType.html
+        )
+        fm = FastMail(conf)
+        await fm.send_message(message,
+                              template_name="reset_password_email.html")
     except ConnectionErrors as err:
         print(f"Failed to send email: {err}")
         raise HTTPException(status_code=500, detail="Email could not be sent.")
