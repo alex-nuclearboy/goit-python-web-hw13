14ea962153644e8d7f2572256a1c560b6243e44a
Aleksander <alex.nuclearboy@gmail.com>
2024-05-20 10:56:20 +0200

    Add using environment variables

--- second_task/quotes_project/quotes_project/settings.py
+++ second_task/quotes_project/quotes_project/settings.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
